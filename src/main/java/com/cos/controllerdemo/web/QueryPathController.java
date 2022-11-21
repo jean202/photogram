30c550dc399f4af7e2d32b3eb9c6436c0427dedb
Yumyumn <hb13pud731+@gmail.com>
2022-11-21 15:37:13 +0900

    16/17강 내용, 8강부터 다시하기

---
+++ src/main/java/com/cos/controllerdemo/web/QueryPathController.java
@@ -0,0 +1,9 @@
+package com.cos.controllerdemo.web;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class QueryPathController {
+//    public String chickenQuery() {
+//    }
+}
